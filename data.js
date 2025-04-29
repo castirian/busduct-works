dd7c64b120c3c2fa8d5f651497d23e5f0b24c049
castirian <castirian@gmail.com>
2025-04-30 00:21:17 +0900

    update0430

--- data.js
+++ data.js
@@ -5,5 +5,36 @@ const projects = [
     photos: [
       "images/2025-05-03/photo1.mp4"
     ]
+  },
+  {
+    date: "2024-10-01",
+    project: "인천 씨티오씨엘 4단지",
+    photos: [
+      "images/2024-10-01/photo1.jpg",
+      "images/2024-10-01/photo2.jpg",
+      "images/2024-10-01/photo3.jpg",
+      "images/2024-10-01/photo4.jpg",
+      "images/2024-10-01/photo5.jpg",
+      "images/2024-10-01/photo6.jpg",
+      "images/2024-10-01/photo7.jpg",
+      "images/2024-10-01/photo8.jpg",
+      "images/2024-10-01/photo9.jpg",
+      "images/2024-10-01/photo10.jpg"
+    ]
+  },
+  {
+    date: "2024-09-25",
+    project: "강원도 원주 레스티지",
+    photos: [
+      "images/2024-09-25/photo1.jpg",
+      "images/2024-09-25/photo2.jpg",
+      "images/2024-09-25/photo3.jpg",
+      "images/2024-09-25/photo4.jpg",
+      "images/2024-09-25/photo5.jpg",
+      "images/2024-09-25/photo6.jpg",
+      "images/2024-09-25/photo7.jpg",
+      "images/2024-09-25/photo8.jpg",
+      "images/2024-09-25/photo9.jpg"
+    ]
   }
 ];
